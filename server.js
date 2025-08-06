28c91b4d6cd876e040900f7667b0057686a6d697
Sumit Bhatt <sumitbhatt9958@gmail.com>
2025-08-06 18:23:02 +0530

    chat feature

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const express = require('express');
-const app = express();
+const http = require('http');
 const bcrypt = require('bcrypt');
 const { v4: uuidv4 } = require('uuid');
 const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001", // The origin of your Next.js app
+    methods: ["GET", "POST"]
+  }
+});
 const port = 3000;
 
 app.use(express.json());
 app.use(cors());
 
-const controlRooms = require("./routes/rooms")
+io.on("connection",(socket)=>{
+    console.log("User connected.");
+
+    socket.on("chat-message", (msg)=>{
+        console.log("received msg "+ msg);
+        io.emit("chat-message", msg);
+    })
+
+    socket.on('disconnect',()=>{
+        console.log("User disconnected.")
+    })
+})
+
+const controlRooms = require("./routes/rooms");
 
 app.use("/rooms", controlRooms)
 
@@ -17,6 +41,6 @@ app.get("/", (req,res)=>{
     res.send("Home page")
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 }); 
